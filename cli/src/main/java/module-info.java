16f9509c2bcb10fef3cf1a988517c066e8b9641b
Jochem Broekhoff <broekhoff.jochem@gmail.com>
2019-12-14 20:24:25 +0100

    Basic project structure.

---
+++ cli/src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module motorscript.cli {
+    requires kotlin.stdlib;
+
+    exports nl.jochembroekhoff.motorscript.cli;
+}
---
+++ cli/src/main/kotlin/nl/jochembroekhoff/motorscript/cli/Main.kt
@@ -0,0 +1,8 @@
+package nl.jochembroekhoff.motorscript.cli
+
+object Main {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println("Hello ebic gamers")
+    }
+}
