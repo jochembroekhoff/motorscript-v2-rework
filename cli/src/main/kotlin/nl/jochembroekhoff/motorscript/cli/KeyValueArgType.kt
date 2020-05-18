08a23361e59f0df103c685e7ddfeeb5ecec9c8cc
Jochem Broekhoff <broekhoff.jochem@gmail.com>
2020-05-18 13:48:29 +0200

    Update to Gradle 6.4.1

--- cli/src/main/kotlin/nl/jochembroekhoff/motorscript/cli/KeyValueArgType.kt
+++ cli/src/main/kotlin/nl/jochembroekhoff/motorscript/cli/KeyValueArgType.kt
@@ -7,17 +7,16 @@ class KeyValueArgType : ArgType<Pair<String, String>>(true) {
     override val description: kotlin.String
         get() = "{ key=value }"
 
-    override val conversion: (value: kotlin.String, name: kotlin.String) -> Pair<kotlin.String, kotlin.String>
-        get() = { rawValue, name ->
-            val split = rawValue.split('=', limit = 2)
-            if (split.size != 2) {
-                throw Exception("Option $name is expected to be in key-value form, separated by an equals sign.")
-            }
-            val key = split[0].trimEnd()
-            val value = split[1].trimStart()
-            if (key.isBlank()) {
-                throw Exception("Option $name is provided with a blank key.")
-            }
-            Pair(key, value)
+    override fun convert(value: kotlin.String, name: kotlin.String): Pair<kotlin.String, kotlin.String> {
+        val split = value.split('=', limit = 2)
+        if (split.size != 2) {
+            throw Exception("Option $name is expected to be in key-value form, separated by an equals sign.")
         }
+        val key = split[0].trimEnd()
+        val associatedValue = split[1].trimStart()
+        if (key.isBlank()) {
+            throw Exception("Option $name is provided with a blank key.")
+        }
+        return Pair(key, associatedValue)
+    }
 }
